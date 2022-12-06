c984f80a0926b48b14075964faec770879e1c578
ZYonash <ZYonash@slb.com>
2022-12-06 12:31:02 -0500

    Day 6 solutions

---
+++ day6/part2.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func allUnique(buffer []string) bool {
+	m := make(map[string]int)
+	for _, c := range buffer {
+		if m[c] == 0 {
+			m[c] = 1
+			continue
+		}
+		if m[c] == 1 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(f)
+	buffer := []string{}
+	for scanner.Scan() {
+		for i, c := range scanner.Text() {
+			buffer = append(buffer, string(c))
+			if len(buffer) == 14 {
+				if allUnique(buffer) {
+					fmt.Println(i + 1)
+					os.Exit(0)
+				} else {
+					buffer = buffer[1:14]
+				}
+			}
+		}
+	}
+}
